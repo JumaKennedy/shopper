0bd1e64fe887bf8e3cb7225586ef99b26f9ce2ec
JumaKennedy <kenn.juma@yahoo.com>
2021-01-16 17:38:28 -0600

    Delete shopper/src directory

--- shopper/src/main/java/com/shop/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shop.model.Products;
-
-public interface ProductRepository extends JpaRepository<Products, Long> {
-
-}
